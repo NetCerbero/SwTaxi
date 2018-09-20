362d84b538ea0b056d0b6590c25b528eba4fb1e5
BlueNet <luiyer1920@gmail.com>
2018-09-20 15:51:33 -0400

    dibujado de ruta correcta

--- src/component/Prueba.js
+++ src/component/Prueba.js
@@ -3,7 +3,11 @@ import MapView, { Marker} from 'react-native-maps';
 import {StyleSheet, View} from 'react-native';
 import {Dimensions} from 'react-native';
 import Toast from 'react-native-simple-toast';
+import MapViewDirections from 'react-native-maps-directions';
 
+
+const {width, height} = Dimensions.get('window');
+const GOOGLE_MAPS_APIKEY = 'AIzaSyBtfZupkTUaGoBT11dC8EBv3yfAMtZmSeQ';
 const style = StyleSheet.create({
 	infoWrapper:{
 		position: 'absolute',
@@ -56,17 +60,36 @@ export default class Prueba extends Component{
 		);
 		this.setState({markers:markers});
 	}
+
+	renderRuta(item, index){
+		if((this.state.markers.length >= 2) && (index < this.state.markers.length - 1) ){
+			return(
+				<MapViewDirections
+					key={index}
+		            origin={this.state.markers[index].coordinate}
+		            destination={this.state.markers[index + 1].coordinate}
+		            apikey={GOOGLE_MAPS_APIKEY}
+		            strokeWidth={3}
+		            strokeColor="hotpink"
+		        />
+		    );
+		}
+		return null;
+	}
+
 	render(){
 		return(
 			<View style={{flex:1}}>
 				<MapView style={style.map}
-					showsUserLocation
+					showsUserLocation={true}
+					followsUserLocation={true}
 					initialRegion={{
 						latitude:-17.867658,
 						longitude: -63.197338,
 						latitudeDelta: 0.02,
 						longitudeDelta: 0.02
 					}}
+					ref={c => this.mapView = c}
 					onPress={(e) => this.addMarker(e.nativeEvent.coordinate)}
 					//onLongPress={(e) => this.addMarker(e.nativeEvent.coordinate)}
 				>
@@ -79,6 +102,7 @@ export default class Prueba extends Component{
 							//image={require('../../assets/icons/car-marker.png')}
 						/>
 					))}
+					{this.state.markers.map(this.renderRuta.bind(this))}
 				</MapView>	
 			</View>
 		);
